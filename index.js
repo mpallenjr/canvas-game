6464f84b39cd783e20c3e27721c7d8ec4b411c82
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-04 13:50:36 -0500

    enemies spawn from all points on right and left side randomly. working

--- index.js
+++ index.js
@@ -85,9 +85,13 @@ const enemies = []
 function spawnEnemies() {
   setInterval(() => {
     const radius = 30
-    const x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
-    const y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
-    
+    let x 
+    let y 
+    if (Math.random() < 5 ) {
+     x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+     y = Math.random() * canvas.height
+    //  y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
     const color = 'green'
     const angle = Math.atan2(
       canvas.height / 2 - y, 
