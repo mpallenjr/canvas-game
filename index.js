034451e23034eb0652b16f296ae0e1bd9c1b1515
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-04 14:16:51 -0500

    enemy and projectile disappear from canvan when radius touch.

--- index.js
+++ index.js
@@ -121,17 +121,18 @@ projectiles.forEach((projectile) => {
   projectile.update()
 })
 
-enemies.forEach((enemy) => {
+enemies.forEach((enemy, index) => {
   enemy.update()
 
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, projectileIndex) => {
 
    const dist = Math.hypot(
 projectile.x - enemy.x,
 projectile.y - enemy.y)
 
 if (dist - enemy.radius - projectile.radius < 1) {
-  console.log('remove from screen')
+  enemies.splice(index, 1)
+  projectiles.splice(projectileIndex, 1)
 } 
   });
 
