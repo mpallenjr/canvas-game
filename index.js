bb177df498a7c644b663e347659a44fddda7aaf8
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-05 13:01:17 -0500

    particles are faded.

--- index.js
+++ index.js
@@ -82,14 +82,17 @@ class Enemy {
     this.radius = radius
     this.color = color
     this.velocity = velocity 
+    this.alpha = 1
     
     }
     draw() {
+      c.save()
+      c.globalAlpha = 0.1
       c.beginPath()
       c.arc(this.x, this.y, this.radius, 0,Math.PI * 2, false)
       c.fillStyle = this.color
       c.fill()
-    
+    c.restore()
     }
     update() {
       this.draw()
