dfb4fcd3e911969be64dacc948dc1048d4cd95bb
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-05 13:23:01 -0500

    add friction to particles.

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ class Enemy {
   
   }
   
-
+const friction = 0.98 
   class Particle {
     constructor(x, y, radius, color, velocity){
     this.x = x
@@ -96,6 +96,9 @@ class Enemy {
     }
     update() {
       this.draw()
+      this.velocity.x *= friction
+      this.velocity.y *= friction
+
     this.x = this.x + this.velocity.x
     this.y = this.y + this.velocity.y
     this.alpha -= 0.01
