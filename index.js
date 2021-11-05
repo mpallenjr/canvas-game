69c726c89b4cec805fa31d023ad0b11dc3e01104
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-05 12:58:13 -0500

    add particles on impact.

--- index.js
+++ index.js
@@ -74,6 +74,33 @@ class Enemy {
   
   }
   
+
+  class Particle {
+    constructor(x, y, radius, color, velocity){
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity 
+    
+    }
+    draw() {
+      c.beginPath()
+      c.arc(this.x, this.y, this.radius, 0,Math.PI * 2, false)
+      c.fillStyle = this.color
+      c.fill()
+    
+    }
+    update() {
+      this.draw()
+    this.x = this.x + this.velocity.x
+    
+    this.y = this.y + this.velocity.y
+    }
+    
+    }
+
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 const player = new Player(x, y, 12, 'white')
@@ -81,7 +108,7 @@ const player = new Player(x, y, 12, 'white')
 
 const projectiles = []
 const enemies = []
-
+const particles = [] 
 
 function spawnEnemies() {
   setInterval(() => {
@@ -119,6 +146,9 @@ function animate() {
   c.fillStyle = 'rgba(0,0,0,0.1)' 
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw()
+  particles.forEach(particle => { 
+    particle.update()
+  })
 projectiles.forEach((projectile, index) => { 
   projectile.update()
 
@@ -153,8 +183,11 @@ projectile.y - enemy.y)
 
 // when projectiles touch enemy
 if (dist - enemy.radius - projectile.radius < 1) 
-  {
-  if (enemy.radius - 10 > 10) {
+  { // where particles will go
+    for (let i = 0; i < 8; i++) {
+      particles.push(new Particle(projectile.x, projectile.y, 3, enemy.color, {x: Math.random() - 0.5, y: Math.random() - 0.5}))
+    }
+  if (enemy.radius - 10 > 6) {
     gsap.to(enemy, {
       radius: enemy.radius - 10
     })
